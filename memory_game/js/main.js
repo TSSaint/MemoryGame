c7f3a8b282f7d943b1453244d67b450791f07f33
TSSaint <thomas@socialeffort.co>
2017-06-11 19:03:38 -0400

    Create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,6 @@
-console.log("User flipped " + cardOne);
-
 var cardOne = "Queen of Diamonds";
 var cardTwo = "King of Diamonds";
 var cardThree = "Queen of Hearts";
 var cardFour = "Queen of Diamonds";
 
+console.log("User flipped " + cardOne);
