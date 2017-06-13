5abf58524220f2882de44110dfac4d9ce44763d5
TSSaint <thomas@socialeffort.co>
2017-06-12 21:30:02 -0400

    Create new game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,23 @@
-var cardOne = "Queen of Diamonds";
-var cardTwo = "King of Diamonds";
-var cardThree = "Queen of Hearts";
-var cardFour = "Queen of Diamonds";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+// empty --> []
 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+// queen
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+// queen and king
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		// same
+		alert("You found a match!");
+	} else {
+		// different
+		alert("Sorry, try again.");
+	}
+}
